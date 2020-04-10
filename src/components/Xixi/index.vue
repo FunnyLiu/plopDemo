e54b9aab90f0f8281fb1cac304819d725643cde9
刘放 <362512489@qq.com>
2020-04-10 17:02:53 +0800

    feat: add demo

---
+++ src/components/Xixi/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div />
+</template>
+
+<script>
+export default {
+  name: 'Xixi',
+  props: {},
+  data() {
+    return {}
+  },
+  created() {},
+  mounted() {},
+  methods: {}
+}
+</script>
+
